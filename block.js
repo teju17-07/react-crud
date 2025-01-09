024e060b5c2e6f09564f6936e0fe7bea72e8f0d7
teju17-07 <tejaswichowdary03@gmail.com>
2025-01-09 10:20:14 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,12 @@
+console.log("start");
+
+{
+var a=10;
+console.log(a);
+
+let b=20;
+console.log(b);
+
+const c=30;
+console.log(c);
+}
---
+++ fun-declaration.js
@@ -0,0 +1,31 @@
+//function demo()
+//{
+//    console.log("hello");
+//}
+//console.log(demo);
+//demo()
+
+//function sum(a,b)
+//{
+//    let c=a+b
+//    return c
+//    console.log("hello");
+//}
+//console.log(sum(10+10,10));//20
+//console.log(sum(100,100));
+
+
+//function arth(a,b){
+//    a =Number(window.prompt("enter the a value"))
+//    b =Number(window.prompt("enter the b value"))
+//   let c=a+b
+//   let d=a-b
+//   let e=a*b
+//   let f=a/b
+//   console.log(c)
+//   console.log(d)
+//   console.log(e)
+//   console.log(f)
+//}
+//arth()
+
